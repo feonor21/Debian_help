493a62052040e2cb9f07d3360a11cb1b0523913d
feonor21 <perez.vincent21@gmail.com>
2019-06-06 13:03:00 +0200

    Update launcher_app.sh

--- launcher_app.sh
+++ launcher_app.sh
@@ -11,17 +11,80 @@
 # Short-Description:  Service for enterprise docker
 # Description:
 ### END INIT INFO
+
+
+lock_first_install="/etc/launcher_app/lock_firstinstall.launcher_app";
+etat_application=0; #0=pas modifier,needupdate=update dispo,needinstall=application pas installer
+app_path="/etc/app_service/"
+app_depot_http="https://github.com/feonor21/laucher_debian9.git"
 set -x #echo on
-lockfirstinstall="/etc/launcher_app/lock_firstinstall.launcher_app";
 
-
-
-install_first_time ()
-{
+install_first_time (){
+    echo "Installation initiale"
     ln -sf $basename  '/etc/init.d/launcher_app.sh';
+    echo "--> creation du lien symbolic dans le init.d"
     apt-get update && apt-get dist-upgrade -y && apt-get install git -y && \
-    touch $lockfirstinstall;
+    touch $lock_first_install;
+    echo "--> Update , distupgrade , install git et creation du lockfirstinstall"
+    echo "->Installation initiale Terminer"
 }
 
+compare_version_app(){
+  echo "Comparaison de l'application par rapport au dépot"
+  if [ -d "$app_path" ]; then
+    cd $app_path;
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    if [ "$BRANCH" = "" ]
+    then
+      etat_application="needinstall"
+      echo "need install"
+    else
+      if [ "$BRANCH" != "master" ]
+      then
+        etat_application="needupdate"
+        echo "need update"
+      else
+        etat_application="0"
+        echo "Update to date"
+      fi
+    fi
+  else
+    etat_application="needinstall"
+    echo "need install"
+  fi
+}
+install_app (){
+  echo "Installation de le l'application"
+  mkdir -p $app_path
+  cd $app_path;
+  git clone $app_depot_http
+}
+update_app(){
+echo ""
+}
 
-if test ! -f $lockfirstinstall; then install_first_time; fi
+case $1 in
+  start)
+    if test ! -f $lock_first_install; then install_first_time; fi
+    compare_version_app
+    case $etat_application in
+      needinstall)
+        install_app
+      ;;
+      needupdate)
+        update_app
+    	;;
+      esac
+	;;
+  stop)
+	;;
+  *)
+  echo ""
+  echo ""
+	echo "Usage: "$basename
+  echo "Make by Feonor"
+  echo ""
+  echo "start|stop"
+	exit 1
+esac
+exit 0
