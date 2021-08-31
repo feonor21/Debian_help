1d64a13437ca485ce26ecdd2f58429a2814b4123
feonor21 <perez.vincent21@gmail.com>
2021-08-31 09:54:20 -0400

    update readme for have command to execute in debian

--- install_system.sh
+++ install_system.sh
@@ -58,7 +58,7 @@ install_docker (){
 
         echo "Set up the repository"
         apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
-        curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        curl -L https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | \
             tee /etc/apt/sources.list.d/docker.list > /dev/null
 
