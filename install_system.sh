f0fe8fe64692ae8ab4199d378d5578c1ab844f3f
vincent perez <perez.vincent21@gmail.com>
2023-02-13 21:02:05 -0500

    Update install_system.sh

--- install_system.sh
+++ install_system.sh
@@ -12,7 +12,7 @@
 ### END INIT INFO
 
 install_ohmyzsh (){
-    read -t 30 -p "Do you wish to install ohmyzsh?(y(default)/n)" -e -i 'n' yn || yn=Y
+    read -t 30 -p "Do you wish to install ohmyzsh? " -e -i 'n' yn || yn=Y
     case $yn in
         [Yy]*) echo "Install ohmyzsh proccessing"
         apt -y install zsh zplug
@@ -23,7 +23,7 @@ install_ohmyzsh (){
 }
 
 install_dockercompose (){
-    read -t 30 -p "Do you wish to install/Update Docker compose?(y(default)/n)" -e -i 'Y' yn || yn=Y
+    read -t 30 -p "Do you wish to install/Update Docker compose? " -e -i 'Y' yn || yn=Y
     case $yn in
         [Yy]*)
         echo "Docker compose Copy file to usr/bin"
@@ -35,7 +35,7 @@ install_dockercompose (){
     esac
 }
 install_docker (){
-    read -t 30 -p "Do you wish to install/Update Docker?(y(default)/n)" -e -i 'Y' yn || yn=Y
+    read -t 30 -p "Do you wish to install/Update Docker? " -e -i 'Y' yn || yn=Y
     case $yn in
         [Yy]*)
         echo "Uninstall old versions of Docker"
@@ -51,7 +51,7 @@ install_docker (){
     esac
 }
 install_git (){
-    read -t 30 -p "Do you wish to install GIT?(y(default)/n)" -e -i 'Y' yn || yn=Y
+    read -t 30 -p "Do you wish to install GIT? " -e -i 'Y' yn || yn=Y
     case $yn in
         [Yy]*) echo "install of GIT proccessing"
         apt-get install git -y
@@ -60,7 +60,7 @@ install_git (){
     esac
 }
 update_system (){
-    read -t 30 -p "Do you wish to update system?(y(default)/n)" -e -i 'Y' yn || yn=Y
+    read -t 30 -p "Do you wish to update system? " -e -i 'Y' yn || yn=Y
     case $yn in
         [Yy]*) echo "update system proccessing"
         apt-get dist-upgrade -y
