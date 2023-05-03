16838008ed12888f27d52085c472d59179c252f7
vincent perez <perez.vincent21@gmail.com>
2023-05-03 07:37:42 -0400

    Update install_system.sh

--- install_system.sh
+++ install_system.sh
@@ -28,7 +28,7 @@ install_dockercompose (){
         [Yy]*)
         echo "Docker compose Copy file to usr/bin"
         rm /usr/local/bin/docker-compose
-        curl -L "https://github.com/docker/compose/releases/download/2.16.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
+        curl -L "https://github.com/docker/compose/releases/download/2.17.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
         chmod +x /usr/local/bin/docker-compose
         ;;
         *);;
